79a307053c33572872158eb8ba472a5daff94af8
Pablo Rizo <prozasoconw4@gmail.com>
2022-03-24 00:04:13 -0600

    carrusel de cartas para detalle de instituciones en home terminado

--- lib/src/controllers/global_controller.dart
+++ lib/src/controllers/global_controller.dart
@@ -1,5 +1,7 @@
 // GlobalContollerGetx
 
+import 'package:fullieapp/src/models/institucion_distancia_model.dart';
+import 'package:fullieapp/src/utils/http_utils.dart';
 import 'package:fullieapp/src/utils/to_tocate.dart';
 import 'package:get/state_manager.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -13,9 +15,17 @@ class GloblalController extends GetxController {
   );
   Rx<GoogleMapController>? mapController;
   RxBool isMapReady = false.obs;
+  RxList<InstitucionDistanciaModel> instituciones =
+      <InstitucionDistanciaModel>[].obs;
+  RxSet<Marker> markers = <Marker>{}.obs;
 
   @override
   void onInit() async {
+    // handleGetInitData();
+    super.onInit();
+  }
+
+  handleGetInitData() async {
     final currentPosition = await ToLocate.determinePosition();
 
     cameraPosition.value = CameraPosition(
@@ -23,8 +33,30 @@ class GloblalController extends GetxController {
       zoom: 15,
     );
 
-    isMapReady.value = true;
+    final institucionesResponse = await Http.get(
+      'instituciones/distancia/${currentPosition.latitude}/${currentPosition.longitude}/120',
+      {},
+    );
 
-    super.onInit();
+    final institucionesJsonArray = institucionesResponse.data as List<dynamic>;
+
+    final institucionesDB = institucionesJsonArray.map((institucion) {
+      final inst = InstitucionDistanciaModel.fromJson(institucion);
+      markers.add(
+        Marker(
+          markerId: MarkerId(inst.idInsitucion.toString()),
+          position: LatLng(inst.latitud!, inst.longitud!),
+          infoWindow: InfoWindow(
+            title: inst.institucion,
+            snippet: '${inst.direccion}',
+          ),
+        ),
+      );
+
+      return inst;
+    }).toList();
+
+    isMapReady.value = true;
+    instituciones.value = institucionesDB;
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:fullieapp/src/components/drawer_info_perfil_component.dart';
 import 'package:fullieapp/src/components/drawer_serch_components.dart';
 import 'package:fullieapp/src/controllers/global_controller.dart';
@@ -21,6 +22,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    gController.handleGetInitData();
   }
 
   @override
@@ -41,10 +43,11 @@ class _HomePageState extends State<HomePage> {
             child: Column(
               children: [
                 SizedBox(
-                  height: size.height * 0.9,
+                  height: size.height * 0.65,
                   child: Obx(
                     () => gController.isMapReady.value
                         ? GoogleMap(
+                            zoomControlsEnabled: false,
                             onTap: (value) {
                               setState(() {
                                 _scrollController.animateTo(
@@ -60,32 +63,281 @@ class _HomePageState extends State<HomePage> {
                             onMapCreated: (contoller) {
                               gController.mapController = contoller.obs;
                             },
+                            markers: gController.markers,
                           )
                         : const Center(
                             child: CircularProgressIndicator(),
                           ),
                   ),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _scrollController.animateTo(
-                        320.0,
-                        curve: Curves.easeOut,
-                        duration: const Duration(milliseconds: 300),
-                      );
-                      isListfocused = true;
-                    });
-                  },
-                  child: Container(
-                    constraints: BoxConstraints(
-                      minHeight: size.height * 0.4,
-                      minWidth: size.width,
+                Obx(
+                  () => Material(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
                     ),
-                    color: Theme.of(context).colorScheme.primary,
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [],
+                    color:
+                        Theme.of(context).colorScheme.primary.withOpacity(0.4),
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          _scrollController.animateTo(
+                            320.0,
+                            curve: Curves.easeOut,
+                            duration: const Duration(milliseconds: 300),
+                          );
+                          isListfocused = true;
+                        });
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          SizedBox(
+                            height: size.height * 0.02,
+                          ),
+                          SizedBox(
+                            height: size.height * 0.3,
+                            width: size.width,
+                            child: PageView.builder(
+                              itemBuilder: (cotext, index) {
+                                final institucion =
+                                    gController.instituciones[index];
+
+                                return Container(
+                                  padding: const EdgeInsets.all(
+                                    10,
+                                  ),
+                                  width: size.width * 0.3,
+                                  height: size.height * 0.15,
+                                  margin: const EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(10),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.3),
+                                        blurRadius: 10,
+                                        spreadRadius: 1,
+                                      ),
+                                    ],
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.stretch,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          ClipRRect(
+                                            borderRadius: BorderRadius.circular(
+                                              10,
+                                            ),
+                                            child: Image.network(
+                                              'https://penguin424.me${institucion.imagen}',
+                                              fit: BoxFit.cover,
+                                              width: size.width * 0.3,
+                                              height: size.height * 0.15,
+                                            ),
+                                          ),
+                                          Container(
+                                            width: size.width * 0.6,
+                                            height: size.height * 0.15,
+                                            // color: Colors.deepOrange,
+                                            child: Column(
+                                              children: [
+                                                Text(
+                                                  institucion.institucion!,
+                                                  style: const TextStyle(
+                                                    fontSize: 14,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                  textAlign: TextAlign.center,
+                                                ),
+                                                Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  children: [
+                                                    RatingBar.builder(
+                                                      itemSize: 22,
+                                                      initialRating: 4,
+                                                      minRating: 1,
+                                                      direction:
+                                                          Axis.horizontal,
+                                                      allowHalfRating: true,
+                                                      itemCount: 5,
+                                                      itemBuilder:
+                                                          (context, _) {
+                                                        return const Icon(
+                                                          Icons.star,
+                                                          color: Colors.amber,
+                                                        );
+                                                      },
+                                                      onRatingUpdate: (rating) {
+                                                        print(rating);
+                                                      },
+                                                    ),
+                                                    const Text('Calidad'),
+                                                  ],
+                                                ),
+                                                Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  children: [
+                                                    RatingBar.builder(
+                                                      itemSize: 22,
+                                                      initialRating: 3,
+                                                      minRating: 1,
+                                                      direction:
+                                                          Axis.horizontal,
+                                                      allowHalfRating: true,
+                                                      itemCount: 5,
+                                                      itemBuilder:
+                                                          (context, _) {
+                                                        return const Icon(
+                                                          Icons.star,
+                                                          color: Colors.amber,
+                                                        );
+                                                      },
+                                                      onRatingUpdate: (rating) {
+                                                        print(rating);
+                                                      },
+                                                    ),
+                                                    const Text('Tiempo'),
+                                                  ],
+                                                ),
+                                                Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  children: [
+                                                    RatingBar.builder(
+                                                      itemSize: 22,
+                                                      initialRating: 2.5,
+                                                      minRating: 1,
+                                                      direction:
+                                                          Axis.horizontal,
+                                                      allowHalfRating: true,
+                                                      itemCount: 5,
+                                                      itemBuilder:
+                                                          (context, _) {
+                                                        return const Icon(
+                                                          Icons.star,
+                                                          color: Colors.amber,
+                                                        );
+                                                      },
+                                                      onRatingUpdate: (rating) {
+                                                        print(rating);
+                                                      },
+                                                    ),
+                                                    const Text('Precio'),
+                                                  ],
+                                                ),
+                                              ],
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      SizedBox(
+                                        height: size.height * 0.02,
+                                      ),
+                                      Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: const [
+                                          Text(
+                                            'Abierto',
+                                            style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.green,
+                                            ),
+                                          ),
+                                          Text('Horarario 00:00am - 23:59pm'),
+                                        ],
+                                      ),
+                                      SizedBox(
+                                        height: size.height * 0.02,
+                                      ),
+                                      SizedBox(
+                                        width: size.width * 0.8,
+                                        height: size.height * 0.04,
+                                        child: PageView(
+                                          controller: PageController(
+                                            viewportFraction: 0.8,
+                                            initialPage: 0,
+                                          ),
+                                          children: [
+                                            Container(
+                                              margin:
+                                                  const EdgeInsets.symmetric(
+                                                horizontal: 8.0,
+                                              ),
+                                              child: ElevatedButton.icon(
+                                                onPressed: () {},
+                                                icon:
+                                                    const Icon(Icons.drive_eta),
+                                                label:
+                                                    const Text('Indicaciones'),
+                                              ),
+                                            ),
+                                            Container(
+                                              margin:
+                                                  const EdgeInsets.symmetric(
+                                                horizontal: 8.0,
+                                              ),
+                                              child: ElevatedButton.icon(
+                                                onPressed: () {},
+                                                icon: const Icon(
+                                                    Icons.list_alt_sharp),
+                                                label: const Text('Cita'),
+                                              ),
+                                            ),
+                                            Container(
+                                              margin:
+                                                  const EdgeInsets.symmetric(
+                                                horizontal: 8.0,
+                                              ),
+                                              child: ElevatedButton.icon(
+                                                onPressed: () {},
+                                                icon: const Icon(Icons.phone),
+                                                label: const Text('Llamar'),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                );
+                              },
+                              itemCount: gController.instituciones.length,
+                            ),
+                          ),
+                          Container(
+                            width: size.width * 0.3,
+                            height: size.height * 0.15,
+                          ),
+                          Container(
+                            width: size.width * 0.3,
+                            height: size.height * 0.15,
+                          ),
+                          Container(
+                            width: size.width * 0.3,
+                            height: size.height * 0.15,
+                          ),
+                          Container(
+                            width: size.width * 0.3,
+                            height: size.height * 0.15,
+                          ),
+                          Container(
+                            width: size.width * 0.3,
+                            height: size.height * 0.15,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
